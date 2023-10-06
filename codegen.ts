71920987c1f4e86439c6e88c5af773f46169d487
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-06 16:38:29 +0900

    codegenをupdate

--- codegen.ts
+++ codegen.ts
@@ -2,12 +2,17 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "https://graphql.anilist.co",
+  documents: ["pages/**/*.{ts,tsx}"],
   generates: {
     "./graphql/": {
       plugins: ["typescript"],
       preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
     },
   },
+  ignoreNoDocuments: true,
 };
 
 export default config;
