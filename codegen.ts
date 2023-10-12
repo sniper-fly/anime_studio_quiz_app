c0583ce752caddd95dd64534f74d341d4a91aae7
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-13 00:16:33 +0900

    codegenをテコ入れ

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "https://graphql.anilist.co",
-  documents: ["pages/**/*.{ts,tsx}"],
+  // documents: ["pages/**/*.{ts,tsx}"],
   generates: {
     "./graphql/": {
       plugins: ["typescript"],
