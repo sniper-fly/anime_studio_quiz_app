4cda3e33bbb52a59cdb23b9a149a8ba04484aad6
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-20 07:20:31 +0900

    QuizのUIを作成

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type LayoutProps = {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <div className="p-4">
+      <div className="p-4 border-b border-gray-200">
         <h1 className="text-4xl font-bold text-center">Aniquiz</h1>
       </div>
       <main>{children}</main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,28 @@
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className="md:flex-row flex-col container flex mx-auto border-b border-gray-200 p-2">
+        <div className="md:ml-5 mx-auto bg-blue-100 text-3xl">
+          Total 30pt
+        </div>
+        <div className="md:ml-auto md:mr-0 mx-auto bg-blue-100 text-3xl">
+          4 / 10
+        </div>
+      </div>
+      <div className="container flex mx-auto border-b border-gray-200 py-3">
+        <div className="mx-auto bg-blue-100">
+          image
+        </div>
+      </div>
+      <div className="container mx-auto flex flex-wrap">
+        {
+          Array(4).fill(null).map((_, i) => (
+            <div className="md:w-1/2 w-full p-4 hover:scale-105">
+              <div className="text-center bg-gray-200 rounded-lg p-8">hoeg</div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
