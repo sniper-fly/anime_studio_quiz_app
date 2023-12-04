7e4f0fbdb06cc2c5221d02d1f388c62300084ea5
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-12-04 18:10:36 +0900

    読み込み画面の改善

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,8 +2,9 @@ import { FC } from "react";
 
 const Loading: FC = () => {
   return (
-    <div className="flex justify-center m-3" aria-label="読み込み中">
-      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+    <div className="flex flex-col m-3" aria-label="読み込み中">
+      <div className="mx-auto animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+      <div className="mx-auto">Now Loading...</div>
     </div>
   );
 };
